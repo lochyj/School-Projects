4bf6c75c49bf610afa36e40b9bd9f5772b8bb9c5
Lachlanjowett <79903111+lochyj@users.noreply.github.com>
2022-02-22 11:37:33 +1100

    Update Base-return.py

---
+++ libs/Base-return.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# Initialize the EV3 brick
+ev3 = EV3Brick()
+
+# defining vars
+a = 1
+motorSpeed = 200
+motorTarget = 100
+distanceSensor = ''
+leftMotor = Motor(Port.B)
+rightMotor = Motor(Port.C)
+Ev3Drive = DriveBase(leftMotor, rightMotor, wheel_diameter=55.5, axle_track=104)
+
+
+# Execute
+if a == 1:
+    Ev3Drive.straight(300)
+    Ev3Drive.turn(90)
+    Ev3Drive.straight(300)
+    Ev3Drive.turn(90)
+    Ev3Drive.straight(300)
+    Ev3Drive.turn(90)
+    Ev3Drive.straight(300)
+    Ev3Drive.turn(90)
+    Ev3Drive.straight(300)
