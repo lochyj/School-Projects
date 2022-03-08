4d041b0a10ad5adc9b8ac74c70cd3e8281e40ff3
Lachlanjowett <79903111+lochyj@users.noreply.github.com>
2022-03-08 11:18:22 +1100

    Update main.py

--- Ev3 project/main.py
+++ Ev3 project/main.py
@@ -10,52 +10,14 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 ev3 = EV3Brick()
 
 # defining vars
-motorSpeed = 100
-leftMotor = Motor(Port.B)
-rightMotor = Motor(Port.C)
-Ev3 = DriveBase(leftMotor, rightMotor, wheel_diameter=55.5, axle_track=104)
+distanceDriven = 0
+motorSpeed = 900
+motorTarget = 0
+armMotor = Motor(Port.A)
 
-def Arm(angle):
-    armMotor.run_target(speed=motorSpeed, target_angle= angle)
-    
-def Return():
-    Ev3.straight(-distanceDriven)
-    Arm(0)
-
-# directions function that take a string and returns a list of operations to be executed by the robot
-def execute(directions):
-    distanceDriven = 0
-    # split the string into a list of strings
-    directions = directions.split()
-    # for each string in the list
-    for direction in directions:
-        # split the string into a list of strings
-        direction = direction.split("(")
-        # remove the remaining brackets
-        direction[1] = direction[1].replace(")", "")
-        # if the first string in the list is "left"
-        if direction[0] == "left":
-            # turn left
-            Ev3.turn(-int(direction[1]))
-            print(int(direction[1]))
-        # if the first string in the list is "right"
-        elif direction[0] == "right":
-            # turn right
-            Ev3.turn(int(direction[1]))
-            print(int(direction[1]))
-        # if the first string in the list is "straight"
-        elif direction[0] == "straight":
-            # drive straight
-            Ev3.straight(int(direction[1]))
-            print(int(direction[1]))
-        
-    
-
-# directions string
-
-directions = "straight(150) right(90) straight(150) right(90) straight(150) right(90) straight(150) right(90) straight(150) right(90)"
-
-
-# Execute
-if True: 
-    execute(directions)
+def Arm():
+    armMotor.run_target(speed=motorSpeed, target_angle= 99999)
+ 
+ 
+while True:
+    Arm()
