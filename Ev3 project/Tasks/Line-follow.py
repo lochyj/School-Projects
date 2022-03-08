a47f95c0bf95a69ca1199e58297f1e8601bf4d67
jeunol <95250931+jeunol@users.noreply.github.com>
2022-03-08 11:46:53 +1100

    Update Line-follow.py

--- Ev3 project/Tasks/Line-follow.py
+++ Ev3 project/Tasks/Line-follow.py
@@ -10,13 +10,14 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Initialize the EV3 brick
 ev3 = EV3Brick()
 
-# defining vars
+# defining variables
 motorSpeed = 100
 motorTarget = 0
 line_sensor = ColorSensor(Port.S1)
 leftMotor = Motor(Port.B)
 rightMotor = Motor(Port.C)
 Ev3 = DriveBase(leftMotor, rightMotor, wheel_diameter=55.5, axle_track=104)
+# defining what is black and white, measures the reflectivity of a surface based on its colour.
 BLACK = 9
 WHITE = 30
 threshold = (BLACK + WHITE) / 2
