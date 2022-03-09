33460dba6d722f9b67bdfd3618a38faf2b04409d
jeunol <95250931+jeunol@users.noreply.github.com>
2022-03-09 17:44:28 +1100

    Update Forward-Back.py

--- Ev3 project/Tasks/Forward-Back.py
+++ Ev3 project/Tasks/Forward-Back.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pybricks-micropython
-# importing the necessary libraries
+# importing the necessary libraries for the code to run
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -11,11 +11,6 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Initialize the EV3 brick
 ev3 = EV3Brick()
 
-# defining vars
-# the speed that the motor spins at (not needed for this scenario but it is good to have)
-motorSpeed = 200
-# MotorTarget is the target position of the motor in mm
-motorTarget = 100
 # Defining the motor ports
 leftMotor = Motor(Port.B)
 rightMotor = Motor(Port.C)
@@ -25,6 +20,7 @@ Ev3Drive = DriveBase(leftMotor, rightMotor, wheel_diameter=55.5, axle_track=104)
 
 # Execute
 # There is a weird bug that makes the robot not respond unless it is in a while or if statement
+# Move forward 1000mm (100cm), then move backward 1000mm
 if True:
     Ev3Drive.straight(1000)
     Ev3Drive.straight(-1000)
