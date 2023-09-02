9dbedd074c588073b7cf504323ebbaef6225584b
Lachlan Jowett <lachlan@jowett.net.au>
2023-09-02 18:41:01 +1000

    using a ui library to fix ui issues

--- Data Science/15puzzle.py
+++ Data Science/15puzzle.py
@@ -1,14 +1,20 @@
 import pygame
+import pygame_gui
+from pygame_gui import UI_BUTTON_PRESSED
+from pygame_gui.elements import UIButton, UILabel
 
-import constants
+from constants import *
 
 pygame.init()
 
 screen = pygame.display.set_mode(size = (WINDOW_HEIGHT, WINDOW_WIDTH), vsync = True)
+manager = pygame_gui.UIManager((WINDOW_HEIGHT, WINDOW_WIDTH))
 
 screen.fill(WHITE)
 
-font = pygame.font.Font('freesansbold.ttf', 32)
+font32 = pygame.font.Font('freesansbold.ttf', 32)
+font24 = pygame.font.Font('freesansbold.ttf', 24)
+font12 = pygame.font.Font('freesansbold.ttf', 12)
 
 pygame.display.set_caption("Lachlan Jowett's 14-15 Puzzle")
 
@@ -16,10 +22,36 @@ def quit_game():
     pygame.quit()
     exit()
 
-def game_scene():
-    ...
+def clamp(number, smallest, largest):
+    return max(smallest, min(number, largest))
 
 options_grid = []
+cell_width = 100
+cell_height = 100
+
+def draw_grid(surface):
+    # This will actually be doubled? TODO: check if it is 2 px or 1 px wide...
+    CELL_BORDER_WIDTH = 1 #px
+
+    for cell_x_position in range(0, GAME_WIDTH, cell_width):
+        for cell_y_position in range((WINDOW_HEIGHT - GAME_HEIGHT), WINDOW_HEIGHT, cell_height):
+
+            cell_border_pygame_object = pygame.Rect(cell_x_position, cell_y_position, cell_width, cell_height)
+
+            pygame.draw.rect(screen, BLACK, cell_border_pygame_object, CELL_BORDER_WIDTH)
+
+def game_scene():
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_game()
+
+        draw_grid(screen)
+
+        pygame.display.update()
+        screen.fill(WHITE)
+
 
 def resize_grid(width: int, height: int) -> None:
     # I really dislike that i cannot get a reference to options_grid and pass it though the function.
@@ -29,118 +61,153 @@ def resize_grid(width: int, height: int) -> None:
     # This may be hard to understand...
     # Why its needed
     # https://docs.python.org/3/library/stdtypes.html#index-20
-    options_grid = [[0 for j in width] for i in height]
-
-def create_text_box(width: int, height: int, border: bool):
-    box_rect = pygame.Rect(width, height)
-
-    box_rect.fill(WHITE)
-
-    if border:
-        pygame.draw.rect(box_rect, BLACK, [0, 0, width, height], 1)
-
-    return box_rect
+    options_grid = [[0 for j in range(width)] for i in range(height)]
 
 
 def options_scene():
     PANEL_WIDTH = 600
-    PANEL_HEIGHT = WINDOW_WIDTH
+    PANEL_HEIGHT = WINDOW_HEIGHT
 
     OPTIONS_WIDTH = 200
-    OPTIONS_HEIGHT = WINDOW_WIDTH
+    OPTIONS_HEIGHT = WINDOW_HEIGHT
 
     screen.fill(WHITE)
 
-    selections_panel = pygame.Surface((OPTIONS_WIDTH, OPTIONS_HEIGHT))
-    grid_panel = pygame.Surface((PANEL_WIDTH, PANEL_HEIGHT))
+    options_panel = pygame_gui.elements.UIPanel(relative_rect = pygame.Rect(0, 0, OPTIONS_WIDTH, OPTIONS_HEIGHT), manager = manager)
 
-    selections_panel.fill(WHITE)
-    grid_panel.fill(WHITE)
+    # create 3 buttons for cell type
 
-    pygame.draw.rect(selections_panel, BLACK, [0, 0, OPTIONS_WIDTH, OPTIONS_HEIGHT], 1)
-    pygame.draw.rect(grid_panel, BLACK, [0, 0, PANEL_WIDTH, PANEL_HEIGHT], 1)
+    # 0 is blank, 1 is number 2 is inactive
+    cell_type = 0
 
-    options_text = font.render("Selection panel", ANTIALIAS, BLACK)
+    blank_cell_button = UIButton(
+        relative_rect = pygame.Rect(0, 0, 150, 150),
+        text = "Blank Cell",
+        manager = manager
+    )
 
-    current_grid_width = 4
-    current_grid_height = 4
+    number_cell_button = UIButton(
+        relative_rect = pygame.Rect(0, OPTIONS_HEIGHT / 3, 150, 150),
+        text = "Regular Cell",
+        manager = manager
+    )
 
-    resize_grid(current_grid_width, current_grid_height)
+    inactive_cell_button = UIButton(
+        relative_rect = pygame.Rect(0, (OPTIONS_HEIGHT / 3) * 2, 150, 150),
+        text = "Inactive Cell",
+        manager = manager
+    )
 
-    width_value = create_text_box(20, 20, True)
+    cells_wide = 7
+    cells_high = 7
+
+    # set the colour of the grid_size_label UILabel to be black text
+
+    # TODO
+
+    grid_size_label = UILabel(
+        relative_rect = pygame.Rect(PANEL_WIDTH + 15, WINDOW_HEIGHT - 300, 200, 100),
+        text = f"Grid size: {cells_wide}x{cells_high}",
+        manager = manager,
+        object_id="#gsl"
+    )
+
+    increase_cells_wide_button = UIButton(
+        relative_rect = pygame.Rect(PANEL_WIDTH + 50, WINDOW_HEIGHT - 100, 50, 50),
+        text = "+ w",
+        manager = manager
+    )
+    decrease_cells_wide_button = UIButton(
+        relative_rect = pygame.Rect(PANEL_WIDTH + 125, WINDOW_HEIGHT - 200, 50, 50),
+        text = "- w",
+        manager = manager
+    )
+
+    increase_cells_high_button = UIButton(
+        relative_rect = pygame.Rect(PANEL_WIDTH + 50, WINDOW_HEIGHT - 200, 50, 50),
+        text = "+ h",
+        manager = manager
+    )
+    decrease_cells_high_button = UIButton(
+        relative_rect = pygame.Rect(PANEL_WIDTH + 125, WINDOW_HEIGHT - 100, 50, 50),
+        text = "- h",
+        manager = manager
+    )
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit_game()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == UI_BUTTON_PRESSED:
+                # handle cell type buttons
+                if event.ui_element == blank_cell_button:
+                    blank_cell_button.focus()
+                    cell_type = 0
+                elif event.ui_element == number_cell_button:
+                    blank_cell_button.focus()
+                    cell_type = 1
+                elif event.ui_element == inactive_cell_button:
+                    blank_cell_button.focus()
+                    cell_type = 2
 
-            if
+                # handle grid size buttons
+                elif event.ui_element == increase_cells_wide_button:
+                    cells_wide = clamp(cells_wide + 1, 2, 14)
 
-        selections_panel.blit(options_text, [OPTIONS_WIDTH / 2 - (options_text.get_width() / 2), 10])
 
-        screen.blit(selections_panel, [WINDOW_WIDTH - OPTIONS_WIDTH, 0])
-        screen.blit(grid_panel, [0, 0])
+                elif event.ui_element == decrease_cells_wide_button:
+                    cells_wide = clamp(cells_wide - 1, 2, 14)
+
+                elif event.ui_element == increase_cells_high_button:
+                    cells_high = clamp(cells_high + 1, 2, 14)
+
+                elif event.ui_element == decrease_cells_high_button:
+                    cells_high = clamp(cells_high - 1, 2, 14)
+
+                grid_size_label.set_text(f"Grid size: {cells_wide}x{cells_high}")
+
+            manager.process_events(event)
+
+        manager.update(1 / 60)
+        manager.draw_ui(screen)
 
         pygame.display.update()
-
         screen.fill(WHITE)
-
 
 
 
 def main_menu():
-    begin = font.render("Begin", ANTIALIAS, BLACK)
-    options = font.render("Options", ANTIALIAS, BLACK)
-    quit = font.render("Quit", ANTIALIAS, BLACK)
+    buttons_rect = pygame.Rect((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2), 200, 200)
 
-    currently_selected_menu_option = 0
+    begin_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((WINDOW_WIDTH / 2) - (buttons_rect.width / 2), 200, buttons_rect.width, buttons_rect.height / 3.5), text = "Begin", manager = manager)
+    options_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((WINDOW_WIDTH / 2) - (buttons_rect.width / 2), 300, buttons_rect.width, buttons_rect.height / 3.5), text = "Options", manager = manager)
+    quit_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((WINDOW_WIDTH / 2) - (buttons_rect.width / 2), 400, buttons_rect.width, buttons_rect.height / 3.5), text = "Quit", manager = manager)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit_game()
+            if event.type == UI_BUTTON_PRESSED:
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    currently_selected_menu_option = max(currently_selected_menu_option - 1, 0)
-                elif event.key == pygame.K_DOWN:
-                    currently_selected_menu_option = min(currently_selected_menu_option + 1, 2)
-                elif event.key == pygame.K_RETURN:
-                    match currently_selected_menu_option:
-                        case 0:
-                            game_scene()
-                        case 1:
-                            options_scene()
-                        case 2:
-                            quit_game()
+                begin_button.kill()
+                options_button.kill()
+                quit_button.kill()
 
-        # This is terrible
+                if event.ui_element == begin_button:
+                    game_scene()
+                elif event.ui_element == options_button:
+                    options_scene()
+                elif event.ui_element == quit_button:
+                    quit_game()
 
-        match currently_selected_menu_option:
-            case 0:
-                begin = font.render("Begin", ANTIALIAS, GREEN)
-                options = font.render("Options", ANTIALIAS, BLACK)
-                quit = font.render("Quit", ANTIALIAS, BLACK)
-            case 1:
-                options = font.render("Options", ANTIALIAS, GREEN)
-                begin = font.render("Begin", ANTIALIAS, BLACK)
-                quit = font.render("Quit", ANTIALIAS, BLACK)
-            case 2:
-                quit = font.render("Quit", ANTIALIAS, GREEN)
-                begin = font.render("Begin", ANTIALIAS, BLACK)
-                options = font.render("Options", ANTIALIAS, BLACK)
+            manager.process_events(event)
 
-        screen.blit(begin, [WINDOW_WIDTH//2 - (begin.get_width() / 2), (WINDOW_HEIGHT // 2) / 3 * 1])
-        screen.blit(options, [WINDOW_WIDTH//2 - (options.get_width() / 2), (WINDOW_HEIGHT // 2) / 3 * 2])
-        screen.blit(quit, [WINDOW_WIDTH//2 - (quit.get_width() / 2), (WINDOW_HEIGHT // 2) / 3 * 3])
+        manager.update(1 / 60)
+        manager.draw_ui(screen)
 
         pygame.display.update()
-
         screen.fill(WHITE)
-
-
-
 
 main_menu()
 
