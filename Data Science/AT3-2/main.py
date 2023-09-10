9e3fc9067589c14f995134aafa762c4c4eece350
Lachlan Jowett <lachlan@jowett.net.au>
2023-09-10 17:54:48 +1000

    feat. number selector / panel working with mouse dragging and clicking

--- Data Science/AT3-2/main.py
+++ Data Science/AT3-2/main.py
@@ -10,11 +10,17 @@ from src.constants import *
 from src.generator import generate_sudoku_grid, print_sudoku_grid
 from src.display import *
 
+
 def main():
     grid = generate_sudoku_grid(MEDIUM)
     # print_sudoku_grid(grid)
 
     window, gui_manager = initialise_window()
+
+    mouse_pos = pygame.mouse.get_pos()
+    drawing_moving_number = False
+    selected_number = None
+
 
     while True:
 
@@ -23,10 +29,20 @@ def main():
                 pygame.quit()
                 exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                drawing_moving_number = True
+                selected_number = handle_number_selector_click(mouse_pos)
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                drawing_moving_number = False
+
+            elif event.type == pygame.MOUSEMOTION:
+                mouse_pos = event.pos
 
         draw_sudoku_grid(window, grid)
         draw_side_bar(window, gui_manager)
+
+        if drawing_moving_number and selected_number != None:
+            draw_moving_number(window, mouse_pos, selected_number)
 
         pygame.display.update()
         window.fill(WHITE)
