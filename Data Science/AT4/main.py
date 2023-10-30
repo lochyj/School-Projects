77cf3239dda9d0aec9e0506db3c69512bd1d5082
Lachlan Jowett <lachlan@jowett.net.au>
2023-10-30 21:58:37 +1100

    work

--- Data Science/AT4/main.py
+++ Data Science/AT4/main.py
@@ -16,6 +16,8 @@ breakdown = []
 
 deltas = []
 
+immune = []
+
 infected = 7769783
 
 r_value = random.uniform(1.4, 2.4)
@@ -24,7 +26,7 @@ breakdown.append(infected)
 
 for i in range(days):
 
-    infected = infected * r_value / 2.2
+    infected = infected * r_value / 2.2 # This is half of the average time to infection, 4.2 is the actual value but it was too high...
 
     delta = infected - breakdown[i]
 
@@ -34,6 +36,9 @@ for i in range(days):
 
     if len(deltas) >= random.randint(10, 21):
         infected -= deltas.pop(0)
+
+    if len(immune) >= random.randint(200, 250): # 243 days is the average immunity time
+        ...
 
     breakdown.append(infected)
     print([delta, infected])
