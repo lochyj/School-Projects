4a818e3982416f3bc3c1f1f2db080e45aafa4074
Lachlan Jowett <lachlan@jowett.net.au>
2023-10-31 14:18:45 +1100

    real recovery

--- Data Science/AT4/main.py
+++ Data Science/AT4/main.py
@@ -10,39 +10,43 @@ from src.constant import *
 # Code |
 # -----|
 
-days = 209
+days = 1009
 
 breakdown = []
 
-deltas = []
-
-immune = []
+deltas = [12, 53, 74, 32, 75, 25, 82, 53]
 
 infected = 7769783
+#infected = 1000
 
-r_value = random.uniform(1.4, 2.4)
+r_value = random.uniform(1.4, 2.6)
 
 breakdown.append(infected)
 
+total = [infected]
+
 for i in range(days):
 
-    infected = infected * r_value / 2.2 # This is half of the average time to infection, 4.2 is the actual value but it was too high...
+    infected = int(infected * max(1, r_value / 2.2)) # This is half of the average time to infection, 4.2 is the actual value but it was too high...
 
     delta = infected - breakdown[i]
 
-    r_value = random.uniform(1.4, 2.4)
+    r_value = random.uniform(1.4, 2.6)
+
+    total.append(total[i] + delta)
 
     deltas.append(delta)
 
     if len(deltas) >= random.randint(10, 21):
         infected -= deltas.pop(0)
 
-    if len(immune) >= random.randint(200, 250): # 243 days is the average immunity time
-        ...
-
     breakdown.append(infected)
     print([delta, infected])
 
 with open("./out/recovery.csv", 'w') as f:
-    for val in breakdown:
-        print(val, file=f)
+    for i in breakdown:
+        print(i, file=f)
+
+with open("./out/extended_recovery.csv", 'w') as f:
+    for i in total:
+        print(i, file=f)
